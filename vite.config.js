56d72ea9760b209d0d247a06e024925b0e8fe191
echo team <abedalrhmnhassan@gmail.com>
2024-12-21 16:04:02 +0200

    upppp

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4000,
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
